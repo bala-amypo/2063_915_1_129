5e5e0f250670da3a1223742734c89802e5695b97
bala-amypo <bala@amypo.in>
2025-12-19 06:33:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/BundleRuleRepository.java
+++ src/main/java/com/example/demo/repository/BundleRuleRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 
-import com.example.demo.model.*;
+import com.example.demo.model.BundleRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
+import java.util.List;
+
+
 public interface BundleRuleRepository extends JpaRepository<BundleRule, Long> {
 List<BundleRule> findByActiveTrue();
 }
--- src/main/java/com/example/demo/repository/CartItemRepository.java
+++ src/main/java/com/example/demo/repository/CartItemRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 
-import com.example.demo.model.*;
+import com.example.demo.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 List<CartItem> findByCartId(Long cartId);
