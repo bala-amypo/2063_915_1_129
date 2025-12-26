2c2c558080b820706a2695890a96a7d20e5c950d
bala-amypo <bala@amypo.in>
2025-12-26 04:45:49 +0000

    User commit

--- src/main/java/com/example/demo/service/BundleRuleService.java
+++ src/main/java/com/example/demo/service/BundleRuleService.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
+
 import com.example.demo.model.BundleRule;
+import java.util.List;
 
 public interface BundleRuleService {
     BundleRule createRule(BundleRule rule);
+    List<BundleRule> getActiveRules();
 }
