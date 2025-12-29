8bc308605485e9a50e1d513749caf63233bf40a2
bala-amypo <bala@amypo.in>
2025-12-29 05:48:32 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -4,13 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
-
-    // Test 49 & 52: Must accept these 3 params and return a string
     public String generateToken(String email, String role, Long userId) {
         return "JWT-TOKEN-" + email + "-" + role + "-" + userId;
     }
-
-    // Test 50 & 51: Logic to return true/false based on token string
     public boolean validateToken(String token) {
         if (token == null || token.isEmpty() || token.equals("invalid")) {
             return false;
